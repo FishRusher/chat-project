e78a5629dbeef552042417d299fd6945a94675df
unknown <rybkar@ELEKTRYK.LOCAL>
2023-10-12 10:52:16 +0200

    login form

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,37 +3,41 @@ import { Box, Button, Paper, TextField } from "@mui/material"
 import {useNavigate} from "react-router-dom"
 
 const LoginPage = () => {
-  let nick = useState("")
-  let pass = useRef("")
+  let nick = useRef(null)
+  let pass = useRef(null)
   let navigate = useNavigate()
 
   function login() {
     if (nick.current === null) return
     if (pass.current === null) return
-    let n = nick.current
-    let p = pass.current
-    console.log(n)
-    console.log(p)
+    let n = nick.current.value
+    let p = pass.current.value
     if (n === "" || n === undefined) return
     if (p === "" || p === undefined) return
-
-
     let data = new FormData()
     data.append("user_nick", n)
     data.append("user_password", p)
 
-    fetch(`${import.meta.env.BACKEND_ADDRESS}/login`, {
+    fetch(`${process.env.REACT_APP_BACKEND_ADDRESS}/login`, {
       method: "POST",
       body: data
     }).then(response => response.json())
     .then(response => {
-      if (response["status"] === "LOGGED") {
-        localStorage.setItem("jwt", response["jwt"])
-        navigate("/")
+      try {
+        if (response["status"] === "LOGGED") {
+          localStorage.setItem("jwt", response["jwt"])
+          navigate("/")
+        }
+        else {
+          // to ADD
+        }
       }
-      else {
-        // to ADD
+      catch(e) {
+        alert("Błąd danych")
       }
+    })
+    .catch(error => {
+      alert("Błąd")
     })
 
   }
@@ -43,10 +47,10 @@ const LoginPage = () => {
       <Paper elevation={5} sx={{ p: 5 }}>
         <Box sx={{ textAlign: "Center", m: 2, fontSize: 30 }}>Logowanie</Box>
         <Box sx={{ m: 2 }}>
-          <TextField label="Nick" ref={nick}></TextField>
+          <TextField label="Nick" inputRef={nick}></TextField>
         </Box>
         <Box sx={{ m: 2 }}>
-          <TextField label="Hasło" ref={pass} type='password'></TextField>
+          <TextField label="Hasło" inputRef={pass} type='password'></TextField>
         </Box>
 
         <Box sx={{ textAlign: 'center' }}>
