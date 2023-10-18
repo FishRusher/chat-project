d4406a581aff88d5f1818fe20936199712ac4130
Rupert <rybkar@ELEKTRYK.LOCAL>
2023-10-18 09:03:06 +0200

    user register form added, some changes to login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import ChatBox from './components/ChatBox';
+import RegisterPage from './pages/RegisterPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,9 +13,11 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage></MainPage>}>
-          <Route path='chat/:user_id' element={<ChatBox></ChatBox>}></Route>
+          <Route path=':user_id' element={<ChatBox></ChatBox>}></Route>
         </Route>
+        <Route path='register' element={<RegisterPage/>}></Route>
         <Route path="login" element={<LoginPage></LoginPage>}></Route>
+        <Route path='*' element={<div>ERROR 404</div>}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
