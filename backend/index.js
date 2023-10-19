c473dc1a1899bff28415298c83391155e05e4d7a
guuf0 <wrkaszuba@o2.pl>
2023-10-19 10:35:33 +0200

    poprawa autoryzacji tokenu

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
 const dotenv = require('dotenv'); 
 const MD5 = require("crypto-js/md5");
+const cors = require("cors")
 
 const app = express()
 
@@ -10,6 +11,7 @@ dotenv.config()
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello from express');
@@ -75,44 +77,44 @@ app.post('/backend/getUsers', (req, res) => {
     let jwtSecretKey = process.env.JWT_SECRET_KEY
     let token = req.body.jwt
     
-    jwt.verify(token, jwtSecretKey, function(err, decoded) {
-        if(err){
-            return res.status(401).send(JSON.stringify(
-                {
-                    status: "TOKEN_EXPIRED"
-                }
-            ))
-        }
+    try {
+        var decoded = jwt.verify(token, jwtSecretKey);
+    } catch(err) {
+        return res.status(401).send(JSON.stringify(
+            {
+                status: "TOKEN_EXPIRED"
+            }
+        ))
+    }
 
-        let nick = decoded.nick
+    let nick = decoded.nick
+      
+    let conn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "chat-project"
+    })
 
-        let conn = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "",
-            database: "chat-project"
-        })
-
-        conn.connect( (err) => {
+    conn.connect( (err) => {
+        if(err)
+            throw err
+        let query = `select * from users where user_nick!='${nick}'`
+        conn.query(query, (err, result, fields) => {
             if(err)
                 throw err
-            let query = `select * from users where user_nick!='${nick}'`
-            conn.query(query, (err, result, fields) => {
-                if(err)
-                    throw err
-                let usersList = []
-                for(let user of result){
-                    usersList.push({"user_id": user.user_id, "user_nick": user.user_nick})
+            let usersList = []
+            for(let user of result){
+                usersList.push({"user_id": user.user_id, "user_nick": user.user_nick})
+            }
+            return res.status(200).send(JSON.stringify(
+                {
+                    status: "OK",
+                    users: usersList
                 }
-                return res.status(200).send(JSON.stringify(
-                    {
-                        status: "OK",
-                        users: usersList
-                    }
-                ))
-            })
+            ))
         })
-    });
+    })
 
 });
 
