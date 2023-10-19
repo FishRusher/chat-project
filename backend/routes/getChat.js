77cff872651ebd9bdb1c71a957c955419acf0484
guuf0 <wrkaszuba@o2.pl>
2023-10-19 12:06:15 +0200

    testing example message

---
+++ backend/routes/getChat.js
@@ -0,0 +1,82 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const dotenv = require('dotenv'); 
+
+const getChat = (req, res) => {
+    let jwtSecretKey = process.env.JWT_SECRET_KEY
+    let token = req.body.jwt
+    let receiver_id = req.body.receiver_id
+    
+    try {
+        var decoded = jwt.verify(token, jwtSecretKey);
+    } catch(err) {
+        return res.status(401).send(JSON.stringify(
+            {
+                status: "TOKEN_EXPIRED"
+            }
+        ))
+    }
+
+    let sender_id = decoded.id
+
+    let conn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "chat-project"
+    })
+
+    conn.connect( (err) => {
+        if(err)
+            throw err
+        let query = `select * from chatroom_message where receiver_id='${receiver_id}' and sender_id='${sender_id}'`
+        conn.query(query, (err, result, fields) => {
+            if(err){
+                return res.status(401).send(JSON.stringify(
+                    {
+                        status: "INVALID_USER"
+                    }
+                ))
+            }
+            let messageList = []
+            for(let message of result){
+                messageList.push({
+                    message_id: message.message_id, 
+                    incoming: false, 
+                    message_content: message.message_content, 
+                    message_date: message.message_date
+                })
+            }
+            
+            let receiver_nick = ""
+
+            let query2 = `select user_nick from users where user_id='${receiver_id}'`
+            conn.query(query2, (err, result, fields) => {
+                if(err){
+                    return res.status(401).send(JSON.stringify(
+                        {
+                            status: "INVALID_USER"
+                        }
+                    ))
+                }
+                if(result.length){
+                    receiver_nick = result[0].user_nick
+                }
+
+            })
+
+            return res.status(200).send(JSON.stringify(
+                {
+                    status: "OK",
+                    receiver_nick: receiver_nick,
+                    messages: messageList
+                }
+            ))
+            
+        })
+    })
+
+
+
+}
+module.exports = { getChat }
