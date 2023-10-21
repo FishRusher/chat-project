85a0bb01bacbdc9ba121a7c896913691c4dbdf53
FishRusher <szprot19@gmail.com>
2023-10-21 15:04:01 +0200

    fixed db connection (close after use)

--- backend/routes/getChat.js
+++ backend/routes/getChat.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv');
 
 const getChat = (req, res) => {
     let jwtSecretKey = process.env.JWT_SECRET_KEY
     let token = req.body.jwt
     let receiver_id = req.body.receiver_id
-    
+
     try {
         var decoded = jwt.verify(token, jwtSecretKey);
-    } catch(err) {
+    } catch (err) {
         return res.status(401).send(JSON.stringify(
             {
                 status: "TOKEN_EXPIRED"
@@ -26,67 +26,71 @@ const getChat = (req, res) => {
         database: "chat-project"
     })
 
-    conn.connect( (err) => {
-        if(err)
+    conn.connect((err) => {
+        if (err)
             throw err
-        
+
         let messageList = []
 
         let query = `select * from chatroom_message where receiver_id='${receiver_id}' and sender_id='${sender_id}'`
         conn.query(query, (err, result, fields) => {
-            if(err){
+            if (err) {
+                conn.end();
                 return res.status(401).send(JSON.stringify(
                     {
                         status: "INVALID_USER"
                     }
                 ))
             }
-            for(let message of result){
+            for (let message of result) {
                 messageList.push({
-                    message_id: message.message_id, 
-                    incoming: false, 
-                    message_content: message.message_content, 
+                    message_id: message.message_id,
+                    incoming: false,
+                    message_content: message.message_content,
                     message_date: message.message_date
                 })
             }
 
             let query2 = `select * from chatroom_message where receiver_id='${sender_id}' and sender_id='${receiver_id}'`
             conn.query(query2, (err, result, fields) => {
-                if(err){
+                if (err) {
+                    conn.end();
                     return res.status(401).send(JSON.stringify(
                         {
                             status: "INVALID_USER"
                         }
                     ))
                 }
-                for(let message of result){
+                for (let message of result) {
                     messageList.push({
-                        message_id: message.message_id, 
-                        incoming: true, 
-                        message_content: message.message_content, 
+                        message_id: message.message_id,
+                        incoming: true,
+                        message_content: message.message_content,
                         message_date: message.message_date
                     })
                 }
-                
+
                 let receiver_nick = ""
-                
+
                 let query3 = `select user_nick from users where user_id='${receiver_id}'`
                 conn.query(query3, (err, result, fields) => {
-                    if(err){
+                    if (err) {
+                        conn.end();
                         return res.status(401).send(JSON.stringify(
                             {
                                 status: "INVALID_USER"
                             }
-                            ))
+                        ))
                     }
-                    if(result.length){
+                    if (result.length) {
                         receiver_nick = result[0].user_nick
                     }
 
-                    messageList.sort(function(a,b){
+                    messageList.sort(function (a, b) {
                         return new Date(a.message_date) - new Date(b.message_date);
                     });
-                        
+
+                    conn.end();
                     return res.status(200).send(JSON.stringify(
                         {
                             status: "OK",
@@ -95,7 +99,7 @@ const getChat = (req, res) => {
                         }
                     ))
                 })
-                    
+
             })
         })
     })
