4ec0a5e80860050ff77e23360395e3acde694e6a
FishRusher <szprot19@gmail.com>
2023-10-25 17:39:36 +0200

    added delete message functionality for button

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -11,7 +11,7 @@ const ChatMessage = ({ message }) => {
             <Box sx={{ maxWidth: "80%", m: 1 }}>
                 <Box sx={{ color: "#909090", textAlign: message.incoming ? "left" : "right" }}>{getStringDate(message.message_date)}</Box>
                 <Box sx={{ backgroundColor: message.incoming ? blue[50] : green[300], p: 1, borderRadius: 2, position: 'relative' }}>
-                    <MessageSettings incoming={message.incoming}></MessageSettings>
+                    <MessageSettings incoming={message.incoming} message_id={message.message_id}></MessageSettings>
                     <Box>
                         {message.message_content}
                     </Box>
--- frontend/src/components/MessageSettings.jsx
+++ frontend/src/components/MessageSettings.jsx
@@ -1,14 +1,47 @@
 import { Delete, ForwardToInbox } from '@mui/icons-material'
 import { Box, IconButton } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const MessageSettings = ({incoming}) => {
-  return (
-    <Box sx={{position: 'absolute', top: 0, left: incoming ?  "100%" : "unset", right: incoming ? "unset" : "100%"}}>
-        {!incoming && <IconButton children={<Delete fontSize='small'></Delete>}></IconButton>}
-        {incoming && <IconButton children={<ForwardToInbox fontSize='small'></ForwardToInbox>}></IconButton>}
-    </Box>
-  )
+const MessageSettings = ({ incoming, message_id }) => {
+    const navigate = useNavigate()
+
+    function deleteMessage() {
+        const jwt = localStorage.getItem("jwt")
+
+        const data = {
+            "jwt": jwt,
+            "message_id": message_id
+        }
+
+        fetch(`${process.env.REACT_APP_BACKEND_ADDRESS}/getUsers`, {
+            method: "DELETE",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json())
+            .then(response => {
+                if (response.status === "TOKEN_EXPIRED") {
+                    localStorage.removeItem("jwt")
+                    navigate("/login")
+                }
+                if (response.status !== "OK") {
+                    alert(response.status)
+                }
+            })
+            .catch(e => {
+
+            })
+    }
+
+    return (
+        <Box sx={{ position: 'absolute', top: 0, left: incoming ? "100%" : "unset", right: incoming ? "unset" : "100%" }}>
+            {!incoming && <IconButton children={<Delete fontSize='small'></Delete>} onClick={deleteMessage}></IconButton>}
+            {incoming && <IconButton children={<ForwardToInbox fontSize='small'></ForwardToInbox>}></IconButton>}
+        </Box>
+    )
 }
 
 export default MessageSettings
