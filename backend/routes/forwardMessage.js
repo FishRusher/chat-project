3e342be1ab166534169a4e0bd5599ca714208824
guuf0 <wrkaszuba@o2.pl>
2023-10-26 11:00:52 +0200

    added INVALID_MESSAGE status

--- backend/routes/forwardMessage.js
+++ backend/routes/forwardMessage.js
@@ -5,7 +5,7 @@ const forwardMessage = (req, res) => {
     let jwtSecretKey = process.env.JWT_SECRET_KEY
     let token = req.body.jwt
     let message_id = req.body.message_id
-    let receiver_ids = req.body.receiver_id
+    let receiver_ids = req.body.forward_to
     let decoded;
     try {
         decoded = jwt.verify(token, jwtSecretKey);
@@ -30,23 +30,43 @@ const forwardMessage = (req, res) => {
     conn.connect((err) => {
         if (err)
             throw err
-        for(let receiver_id of receiver_ids){
-            let query = `insert into forwarded_message(message_id, sender_id, receiver_id) values (${message_id}, ${sender_id}, ${receiver_id})`
-            conn.query(query, (err, result, fields) => {
-                if (err) {
-                    conn.end()
-                    return res.status(500).send(JSON.stringify(
-                        {
-                            status: "ERROR",
-                        }))
+        
+        let query = `select * from chatroom_message where message_id='${message_id}'`
+        conn.query(query2, (err, result, fields) => {
+            if (err) {
+                conn.end()
+                return res.status(500).send(JSON.stringify(
+                    {
+                        status: "ERROR",
+                    }))
+            }
+            if(result){
+                for(let receiver_id of receiver_ids){
+                    let query2 = `insert into forwarded_message(message_id, sender_id, receiver_id) values (${message_id}, ${sender_id}, ${receiver_id})`
+                    conn.query(query2, (err) => {
+                        if (err) {
+                            conn.end()
+                            return res.status(500).send(JSON.stringify(
+                                {
+                                    status: "ERROR",
+                                }))
+                        }
+                    })
                 }
-            })
-        }
-        conn.end()
-        return res.status(200).send(JSON.stringify(
-            {
-                status: "OK",
-            }))
+                conn.end()
+                return res.status(200).send(JSON.stringify(
+                    {
+                        status: "OK",
+                    }))
+            }
+            else{
+                conn.end()
+                return res.status(404).send(JSON.stringify(
+                    {
+                        status: "INVALID_MESSAGE",
+                    }))
+            }
+        })
     })
 }
 module.exports = { forwardMessage }
